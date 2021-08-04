a8e05acfe3343a4d774148f816b4d37598af22b3
unknown <jeonmogeon@gmail.com>
2021-08-04 14:31:08 +0900

    'asdf'

--- src/HackGame.py
+++ src/HackGame.py
@@ -9,6 +9,14 @@ import subprocess
 import custom
 import os
 
+### Version Control ###
+
+
+
+
+
+### ### ### ### ### ###
+
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
